1d5ef0f42ad16b6e9b8ec364c4a4bf869b87de63
GitHub Action <action@github.com>
2026-03-06 17:10:27 +0000

    🎮 Auto-build: Added new exercise(s)

---
+++ exercises/react-example/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'slovosled-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((fetchResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    })
+  );
+});
